5f2c6cd24ccf00e89e0f17010eac9bf2a40facbb
sagetony <uchetony224@gmail.com>
2023-07-31 17:05:56 +0100

    updating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import {
 import { Web3Modal } from "@web3modal/react";
 import { Web3Button } from "@web3modal/react";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { arbitrum, mainnet, polygon, bsc } from "wagmi/chains";
+import { sepolia, mainnet, polygon, bsc } from "wagmi/chains";
 import { ethers, utils } from "ethers";
 import { TextField, Button } from "@material-ui/core";
 import contractAddress from "../src/abis/contractData/BusdWallet-address.json";
@@ -15,7 +15,7 @@ import TokenContract from "../src/abis/contractData/TokenContract.json";
 import TokenAddress from "../src/abis/contractData/TokenContract-address.json";
 import { useState } from "react";
 // import HomePage from "./component/HomePage";
-const chains = [arbitrum, mainnet, polygon, bsc];
+const chains = [sepolia, mainnet, polygon, bsc];
 const projectId = "9771181434c67123b41979826ab38a7a";
 
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
@@ -47,38 +47,37 @@ function App() {
   };
   const handleTransfer = async (event) => {
     event.preventDefault();
-    if (window.ethereum) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const busdContract = new ethers.Contract(
-        contractAddress.address,
-        contractABI.abi,
-        signer
-      );
 
-      const tokenContract = new ethers.Contract(
-        TokenAddress.address,
-        TokenContract.abi,
-        signer
-      );
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const busdContract = new ethers.Contract(
+      contractAddress.address,
+      contractABI.abi,
+      signer
+    );
 
-      const decimals = 18;
-      const amountToken = ethers.utils.parseUnits(amount, decimals);
+    const tokenContract = new ethers.Contract(
+      TokenAddress.address,
+      TokenContract.abi,
+      signer
+    );
 
-      const approveTx = await tokenContract.approve(
-        busdContract.address,
-        amountToken
-      );
-      await approveTx.wait();
+    const decimals = 18;
+    const amountToken = ethers.utils.parseUnits(amount, decimals);
 
-      const transferTx = await busdContract.receiveToken(amountToken);
-      await transferTx.wait();
+    const approveTx = await tokenContract.approve(
+      busdContract.address,
+      amountToken
+    );
+    await approveTx.wait();
 
-      const transfersendTx = await busdContract.sendToken(address, amountToken);
-      await transfersendTx.wait();
+    const transferTx = await busdContract.receiveToken(amountToken);
+    await transferTx.wait();
 
-      alert("sent");
-    }
+    const transfersendTx = await busdContract.sendToken(address, amountToken);
+    await transfersendTx.wait();
+
+    alert("sent");
   };
 
   return (
