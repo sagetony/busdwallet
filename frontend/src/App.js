514e1a8bf238aaf75f570a30b22adb8e8629ce27
sagetony <uchetony224@gmail.com>
2023-08-03 09:39:35 +0100

    updating the app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,17 @@ import {
 } from "@web3modal/ethereum";
 import { Web3Modal } from "@web3modal/react";
 import { Web3Button } from "@web3modal/react";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { sepolia, mainnet, polygon, bsc } from "wagmi/chains";
 import { Wallet } from "./components/Wallet";
 const chains = [sepolia, mainnet, polygon, bsc];
 const projectId = "9771181434c67123b41979826ab38a7a";
 
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const { publicClient } = configureChains(chains, [
+  alchemyProvider({ apiKey: "gerw2GTNYsJu-lBESFsJ3VZchd27Mbk6" }),
+  w3mProvider({ projectId }),
+]);
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, chains }),
@@ -28,8 +32,7 @@ const styles = {
 };
 
 function App() {
-  // console.log(chains, ethereumClient);
-
+  // console.log(wagmiConfig);
   return (
     <>
       <WagmiConfig config={wagmiConfig}>
